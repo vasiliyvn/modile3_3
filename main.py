61db97643f637ab631964557a201b663c7f2a51c
vasiliyvn <mishadasha1114@gmail.com>
2024-08-03 14:59:38 +0500

    Add files via upload

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
-def print_params(a, b, c):
+def print_params(a=1, b='строка', c=True):
     print(a, b, c)
 
 
 print_params(1, 'строка', True)
-value_list_ = [1, 2, 3]
-value_dict_ = {'a': 1, 'b': 2, 'c': 3}
-value_list2_ = [54.32, 'Строка']
+print_params()
+print_params(b=25)
+print_params(c=[1, 2, 3])
+value_list_ = ['bot', False, 3]
+value_dict_ = {'a': True, 'b': 'строка', 'c': 3}
 print_params(*value_list_)
 print_params(**value_dict_)
+value_list2_ = [54.32, 'Строка']
 print_params(*value_list2_, 42)
